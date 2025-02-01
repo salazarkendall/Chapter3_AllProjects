bd9cbc1bf80be803db00b515b4898d98c172fb7c
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-01 12:13:24 -0600

    updating the contents

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -1,9 +1,42 @@
 ﻿Console.WriteLine("**** Fun with Basic Data Types ****");
-LocalVarDeclarations();
+//DefaultDeclarations();
+//LocalVarDeclarations();
+NewingDataTypes();
+
+static void NewingDataTypes()
+{
+    // Use default instead of new bool() for better readability and efficiency,
+    // as 'default' avoids an unnecessary constructor call.
+    Console.WriteLine("-> Using 'new' to create Data Types");
+    bool b = new bool(); // set to false.
+    int i = new int(); // set to 0.
+    double d = new double(); // set to 0.
+    DateTime dt = new DateTime(); // set to 1/1/0001 12:00:00 AM
+
+    Console.WriteLine("Default values (using 'new' keyword)");
+    Console.WriteLine("bool: {0}", b);
+    Console.WriteLine("int: {0}", i);
+    Console.WriteLine("double: {0}", d);
+
+    // You can achieve the same result using the 'new()' functionality in C# 9.0
+    // Just like this:
+    bool b2 = new();
+    int i2 = new();
+    double d2 = new();
+    DateTime dt2 = new();
+
+    Console.WriteLine("Default values (using 'new()' functionality)");
+    Console.WriteLine("bool: {0}", b2);
+    Console.WriteLine("int: {0}", i2);
+    Console.WriteLine("double: {0}", d2);
+    Console.WriteLine("DateTime: {0}", dt2);
+}
 
 static void DefaultDeclarations()
 {
-
+    Console.WriteLine("-> Dafult Declarations");
+    int myInt = default;
+    Console.WriteLine(myInt);
 }
 
 static void LocalVarDeclarations()
