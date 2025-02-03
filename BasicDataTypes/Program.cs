b0ec64473a806472b5e93cadf3d8f98da8809190
salazarkendall <kendallsalazarvargas@gmail.com>
2025-02-02 20:02:50 -0600

    added: UseBigInteger

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("**** Fun with Basic Data Types ****");
+﻿using System.Numerics;
+
+Console.WriteLine("**** Fun with Basic Data Types ****");
 
 //DefaultDeclarations();
 //LocalVarDeclarations();
@@ -9,7 +11,19 @@
 //ParseFromStrings();
 // ParseFromStringWithTryParse();
 // UseDateAndTime();
-UseDateOnlyAndTimeOnly();
+// UseDateOnlyAndTimeOnly();
+UseBigInteger();
+
+static void UseBigInteger()
+{
+    Console.WriteLine("=> Use BigInteger:");
+    BigInteger biggy = BigInteger.Parse("9999999999999999999999999999999999999999999999");
+    Console.WriteLine("Value of biggy is {0}", biggy);
+    Console.WriteLine("Is biggy an even value?: {0}", biggy.IsEven);
+    Console.WriteLine("Is biggy a power of two?: {0}", biggy.IsPowerOfTwo);
+    BigInteger reallyBig = BigInteger.Multiply(biggy, BigInteger.Parse("8888888888888888888888888888888888888888888"));
+    Console.WriteLine("Value of reallyBig is {0}", reallyBig);
+}
 
 static void UseDateOnlyAndTimeOnly()
 {
