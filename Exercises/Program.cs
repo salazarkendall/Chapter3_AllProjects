be07e55419eb847af7d2ec07ce55002d338b5c7a
salazarkendall <kendallsalazarvargas@gmail.com>
2025-02-03 09:37:16 -0600

    added: Exercise2

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -9,8 +9,10 @@ Exercise1();
 // Display the results on the console.
 static void Exercise1()
 {
+  Console.WriteLine("Exercise1");
   Console.WriteLine("Please enter a very large number (more than 30 digits):");
-  if (BigInteger.TryParse(Console.ReadLine(), out BigInteger result))
+  var input = Console.ReadLine();
+  if (input?.Length > 30 && BigInteger.TryParse(input, out BigInteger result))
   {
     Console.WriteLine("BIGINT: {0}", result);
     Console.WriteLine("Is this number even? {0}", result.IsEven);
@@ -20,4 +22,13 @@ static void Exercise1()
   {
     Console.WriteLine("Not a valid number");
   }
+}
+
+// Exercise 2:
+// Define two large numbers as BigInteger.
+// Perform and display the results of the following operations between them:
+// Addition, subtraction, multiplication, division, and modulus.
+static void Exercise2()
+{
+  BigInteger bigInt1 =
 }
