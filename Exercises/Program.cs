828a690f720e02d764def989b5ec0c761b0ffbd5
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-03 10:37:30 -0600

    added: latest changes

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Numerics;
 
-Exercise1();
-Exercise2();
+// Exercise1();
+// Exercise2();
+Exercise3();
 
 // Exercise 1:
 // Ask the user to enter a very large number (more than 30 digits).
@@ -10,7 +11,7 @@ Exercise2();
 // Display the results on the console.
 static void Exercise1()
 {
-  Console.WriteLine("---Exercise1---");
+  Console.WriteLine("--- Exercise 1 ---");
   Console.WriteLine("Please enter a very large number (more than 30 digits):");
   var input = Console.ReadLine();
   if (input?.Length > 30 && BigInteger.TryParse(input, out BigInteger result))
@@ -23,7 +24,7 @@ static void Exercise1()
   {
     Console.WriteLine("-> Not a valid number");
   }
-    Console.ReadLine();
+  Console.ReadLine();
 }
 
 // Exercise 2:
@@ -32,14 +33,30 @@ static void Exercise1()
 // Addition, subtraction, multiplication, division, and modulus.
 static void Exercise2()
 {
-    Console.WriteLine("Excercise2");
-    string fakeNumber = "999999999999999999999999999999";
-    _ = BigInteger.TryParse(fakeNumber, out BigInteger bigInt1);
-    _ = BigInteger.TryParse(fakeNumber, out BigInteger bigInt2);
-    Console.WriteLine("The fake number is: {0}", fakeNumber);
-    Console.WriteLine("-> Addition:       {0}", BigInteger.Add(bigInt1, bigInt2));
-    Console.WriteLine("-> Subtraction:    {0}", BigInteger.Subtract(bigInt1, bigInt2));
-    Console.WriteLine("-> Multiplication: {0}", BigInteger.Multiply(bigInt1, bigInt2));
-    Console.WriteLine("-> Division:       {0}", BigInteger.Divide(bigInt1, bigInt2));
-    Console.ReadLine();
+  Console.WriteLine("--- Excercise 2 ---");
+  string fakeNumber = "999999999999999999999999999999";
+  _ = BigInteger.TryParse(fakeNumber, out BigInteger bigInt1);
+  _ = BigInteger.TryParse(fakeNumber, out BigInteger bigInt2);
+  Console.WriteLine("The fake number is: {0}", fakeNumber);
+  Console.WriteLine("-> Addition:       {0}", BigInteger.Add(bigInt1, bigInt2));
+  Console.WriteLine("-> Subtraction:    {0}", BigInteger.Subtract(bigInt1, bigInt2));
+  Console.WriteLine("-> Multiplication: {0}", BigInteger.Multiply(bigInt1, bigInt2));
+  Console.WriteLine("-> Division:       {0}", BigInteger.Divide(bigInt1, bigInt2));
+  Console.ReadLine();
+}
+
+// Exercise 3
+// Create a program that calculates the factorial of a large number (e.g., 100!) using BigInteger.
+static void Exercise3()
+{
+  Console.WriteLine("--- Exercise 3 ---");
+  Console.WriteLine("Please enter a number (we are going to make a factorial out of it!)");
+  _ = int.TryParse(Console.ReadLine(), out int baseNumber);
+  BigInteger bigNumber = default;
+  Console.WriteLine(bigNumber);
+  for (int i = 0; i <= baseNumber; i++)
+  {
+
+  }
+  Console.WriteLine("-> {0}! == {1}", baseNumber, bigNumber);
 }
