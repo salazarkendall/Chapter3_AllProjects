6ea9495dcbe002985b62e9317ff1ae321efa1243
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-08 12:16:48 -0600

    added: more methods

--- IterationsAndDecisions/Program.cs
+++ IterationsAndDecisions/Program.cs
@@ -6,6 +6,80 @@ string[] names = { "Kendall", "Alina", "Gilo", "Xinia", "Marcela", "Ronald" };
 //JumpingStatements.GotoStatement(names);
 //JumpingStatements.ReturnStatement(names);
 //JumpingStatements.ThrowStatement(names);
+//IfElsePatternMatching();
+//IfElsePatternMatchingUpdatedInCSharp9();
+Console.WriteLine(FromRainbow("Red"));
+Console.ReadLine();
+
+static string FromRainbow(string colorBand)
+{
+    return colorBand switch
+    {
+        "Red" => "#FF0000",
+        "Orange" => "#FF7F00",
+        "Yellow" => "#FFFF00",
+        "Green" => "#00FF00",
+        "Blue" => "#0000FF",
+        "Indigo" => "#4B0082",
+        "Violet" => "#9400D3",
+        _ => "#FFFFFF",
+    };
+}
+
+static void TernaryOperator()
+{
+    string stringData = "My textual data";
+    Console.WriteLine(stringData.Length > 0
+    ? "string is greater than 0 characters"
+    : "string is not greater than 0 characters");
+    Console.WriteLine();
+
+    // compiller error
+    //stringData.Length > 0
+    //? Console.WriteLine("string is greater than 0 characters")
+    //: Console.WriteLine("string is not greater than 0 characters");
+}
+
+static void IfElsePatternMatchingUpdatedInCSharp9()
+{
+    Console.WriteLine("======= C# 9 If Else Pattern Matching Improvements =======");
+    
+    object testItem1 = 123;
+    Type t = typeof(string);
+    char c = 'f';
+    //Type patterns
+    if (t is Type)
+    {
+        Console.WriteLine($"{t} is a Type");
+    }
+    //Relational, Conjuctive, and Disjunctive patterns
+    if (c is >= 'a' and <= 'z' or >= 'A' and <= 'Z')
+    {
+        Console.WriteLine($"{c} is a character");
+    };
+    //Parenthesized patterns
+    if (c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') or '.' or ',')
+    {
+        Console.WriteLine($"{c} is a character or separator");
+    };
+    //Negative patterns
+    if (testItem1 is not string)
+    {
+        Console.WriteLine($"{testItem1} is not a string");
+    }
+    if (testItem1 is not null)
+    {
+        Console.WriteLine($"{testItem1} is not null");
+    }
+    Console.WriteLine();
+}
+
+static void IfElsePatternMatching()
+{
+    object test1 = "String 1";
+    if(test1 is string) Console.WriteLine($"test1 is string: {test1}");
+    if(test1 is string myTest1) Console.WriteLine($"test1 is string: {test1}, {myTest1}");
+}
 
 static class JumpingStatements
 {
