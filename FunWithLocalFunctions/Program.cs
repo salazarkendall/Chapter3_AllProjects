a39e37a46b67451a5bc202d13784f0844f23cdd5
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-10 13:07:59 -0600

    added: latest changes

--- FunWithLocalFunctions/Program.cs
+++ FunWithLocalFunctions/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int AddWrapper(int x, int y)
+{
+  //Do some validation here
+  return Add();
+  int Add()
+  {
+    return x + y;
+  }
+}
+
+static int AddWrapperWithSideEffect(int x, int y)
+{
+  //Do some validation here
+  return Add();
+  int Add()
+  {
+    x += 1;
+    return x + y;
+  }
+}
