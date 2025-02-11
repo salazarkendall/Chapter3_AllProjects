ed8a99ad4a21d1869efc027d7a7ee7eac16144e7
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-11 16:02:40 -0600

    added: enums

---
+++ FunWithEnumns/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("---- Fun with Enums ----");
+
+// Make an EmployeeTypeEnum variable.
+AskForBonus(EmployeeTypeEnum.Contractor);
+Console.ReadLine();
+// Enums as parameters.
+static void AskForBonus(EmployeeTypeEnum employeeType)
+{
+  switch (employeeType)
+  {
+    case EmployeeTypeEnum.Manager:
+      Console.WriteLine("How about stock options instead?");
+      break;
+    case EmployeeTypeEnum.Grunt:
+      Console.WriteLine("You have got to be kidding...");
+      break;
+    case EmployeeTypeEnum.Contractor:
+      Console.WriteLine("You already get enough cash...");
+      break;
+    case EmployeeTypeEnum.VicePresident:
+      Console.WriteLine("VERY GOOD, Sir!");
+      break;
+  }
+}
+
+enum EmployeeTypeEnum
+{
+  Manager,       // 0
+  Grunt,         // 1
+  Contractor,    // 2
+  VicePresident, // 3
+}
+
+enum CustomOrderEmployeeTypeEnum
+{
+  Manager = 100, // 100
+  Grunt,         // 101
+  Contractor,    // 102
+  VicePresident, // 103
+}
+
+enum NotSequentialEmployeeTypeEnum
+{
+  Manager = 100,     // 100
+  Grunt = 9,         // 9
+  Contractor = 1,    // 1
+  VicePresident = 0, // 0
+}
+
+// By default, the storage type used to hold the values of an enumeration is a System.Int32
+// If you want to set the underlying storage value of EmployeeTypeEnum to be a byte rather than an int
+// Numbers must be inside type's range
+enum ByteEmployeeTypeEnum : byte
+{
+  Manager,       // 0
+  Grunt,         // 1
+  Contractor,    // 2
+  VicePresident, // 3
+}
