fd213f60f1ce821dd6fb65ee9474f99ebf364cea
Kendall Salazar <kendallsalazarvargas@gmail.com>
2025-02-14 12:24:51 -0600

    added: practice exercises

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -3,21 +3,54 @@
 // Declaracion e inicializaciones
 
 // Declaracion, asignacion e inicializacion por separado
-//int[] numeros; // Declaracion de la variable
-//numeros = new int[2]; // Asignacion de espacio fijo
-//numeros[0] = 0; // Llenado de espacios
-//numeros[1] = 0; // Llenado de espacios
+//int[] numbers; // Declaracion de la variable
+//numbers = new int[2]; // Asignacion de espacio fijo
+//numbers[0] = 0; // Llenado de espacios
+//numbers[1] = 0; // Llenado de espacios
 
-//int[] numeros = new[]{ 1, 2, 3, 4 }; // Inicializacion directa (con new)
-//int[] numeros = { 1, 2, 3, 4 }; // Inicializacion implicita (sin new)
-//int[] numeros = [1, 2, 3, 4]; // Inicializacion implicita en C# 12 (nueva sintaxis)
-var numeros = new int[1, 2, 3, 4]; // Inicializacion usando var
-                                   //var numeros = [1, 2, 3, 4]; // A partir de C# 12
-//int[] varios = new int[5];
-//Console.WriteLine(varios[3]);
-//Console.WriteLine(varios[3].GetType().ToString()); // Asigna el valor default cuando no ha sido especificado
+//int[] numbers = new[]{ 1, 2, 3, 4 }; // Inicializacion directa (con new)
+//int[] numbers = { 1, 2, 3, 4 }; // Inicializacion implicita (sin new)
+//int[] numbers = [1, 2, 3, 4]; // Inicializacion implicita en C# 12 (nueva sintaxis)
+var numbers = new int[1, 2, 3, 4]; // Inicializacion usando var
+//var numbers = [1, 2, 3, 4]; // A partir de C# 12
+//int[] someNumbers = new int[5];
+//Console.WriteLine(someNumbers[3]);
+//Console.WriteLine(someNumbers[3].GetType().ToString()); // Asigna el valor default cuando no ha sido especificado
 
-//foreach (int numero in numeros)
+//foreach (int number in numbers)
 //{
-//    Console.WriteLine(numero);
+//    Console.WriteLine(number);
 //}
+
+// Arrays multinivel:
+// -> Array rectangular (Bidimensional)
+int[,] matrix1 = new int[3,3];
+matrix1[1, 1] = 10000;
+Console.WriteLine(matrix1[1, 1]);
+
+// -> Jagged array
+int[][] matrix2 = new int[3][];
+matrix2[0] = [1, 2, 3, 4];
+matrix2[1] = [5, 6, 7];
+matrix2[2] = [9, 0, 1, 2, 8];
+
+
+// Indices y Rangos
+string[] phrase = ["Hola", "don", "Pepito", "hola", "don", "Jose"];
+
+Console.WriteLine("Entire phrase:\n");
+foreach (string word in phrase[..])
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine("New phrase:\n");
+foreach(string word in phrase[3..])
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine("\nLast Word");
+Console.WriteLine(phrase[^1]);
+
+
