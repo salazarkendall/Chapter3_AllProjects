2dadb9539c091bd19ec81e2b9285f5c9ffadf4ff
salazarkendall <kendallsalazarvargas@gmail.com>
2025-02-21 10:05:07 -0600

    end: FunWithNullables

---
+++ FunWithNullableRefTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿string? nullableString = null;
+TestClass? myNullableClass = null;
+
+TestClass myNonNullableClass = null; // Converting null literal or possible null value to non-nullable type. (CS8600)
+
+#nullable disable
+TestClass anotherNullableClass = null;
+//Warning CS8632 The annotation for nullable reference types
+//should only be used in code within a '#nullable' annotations
+TestClass? badDefinition = null;
+//Warning CS8632 The annotation for nullable reference types
+//should only be used in code within a '#nullable' annotations
+string? anotherNullableString = null;
+#nullable restore
+
+string? msg = null;
+EnterLogData(msg);
+//Warning CS8604 Possible null reference argument for parameter 'message' in
+//      'void EnterLogData(string message, string owner = "Programmer")'.
+
+static void EnterLogData(string message, string owner = "Programmer")
+{
+  ArgumentNullException.ThrowIfNull(message);
+  Console.WriteLine("Error: {0}", message);
+  Console.WriteLine("Owner of Error: {0}", owner);
+}
+public class TestClass
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+}
---
+++ FunWithNullableValueTypes/Program.cs
@@ -0,0 +1,72 @@
+﻿LocalNullableVariables();
+LocalNullableVariablesUsingNullable();
+
+DataBaseReader dbReader = new();
+
+int myData = dbReader.GetNumericFromDB() ?? 100; // assign 100 to myData if dbReader.GetNumericFromDB() returns null
+Console.WriteLine($"myData -> {myData}");
+
+int? myNullableInt = null;
+myNullableInt ??= 14;
+myNullableInt ??= 22;
+Console.WriteLine($"myNullableInt -> {myNullableInt}");
+
+
+// Get int from database
+var possibleInt = dbReader.GetNumericFromDB();
+if (possibleInt.HasValue)
+{
+  Console.WriteLine($"possibleInt -> {possibleInt.Value}");
+}
+else
+{
+  Console.WriteLine("possibleInt has no value yet");
+}
+// Get bool from database
+var possibleBool = dbReader.GetBoolFromDB();
+if (possibleBool != null)
+{
+  Console.WriteLine($"possibleBool -> {possibleBool.Value}");
+}
+else
+{
+  Console.WriteLine("possibleBool has no value yet");
+}
+
+// TesterMethod(); 
+
+static void TesterMethod(string?[] args)
+{
+  // We should check for null before accessing the array data!
+  Console.WriteLine($"You sent me {args?.Length ?? 0} arguments.");
+}
+
+static void LocalNullableVariables()
+{
+  bool? nullableBool = null;
+  double? nullableDouble = null;
+  int? nullableInt = 10;
+  char? nullableChart = 'a';
+  int?[] nullableArray = new int?[10];
+  Console.WriteLine($"{nullableBool}, {nullableDouble}, {nullableInt}, {nullableChart}, {nullableArray}");
+  if (nullableChart.HasValue) Console.WriteLine($"nullableChart -> {nullableChart}");
+}
+
+static void LocalNullableVariablesUsingNullable()
+{
+  Nullable<bool> nullableBool = null;
+  Nullable<double> nullableDouble = null;
+  Nullable<int> nullableInt = 10;
+  Nullable<char> nullableChart = 'a';
+  Nullable<int>[] nullableArray = new int?[10];
+  Console.WriteLine($"{nullableBool}, {nullableDouble}, {nullableInt}, {nullableChart}, {nullableArray}");
+  if (nullableChart.HasValue) Console.WriteLine($"nullableChart -> {nullableChart}");
+}
+
+class DataBaseReader
+{
+  public int? numericValue;
+  public bool? boolValue = true;
+  public int? GetNumericFromDB() { return numericValue; }
+  public bool? GetBoolFromDB() { return boolValue; }
+}
