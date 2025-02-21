67fbc33fc62109d2d9e58667ea177232ba83ad77
salazarkendall <kendallsalazarvargas@gmail.com>
2025-02-21 00:46:04 -0600

    added: some more exercises

--- Exercises2/Program.cs
+++ Exercises2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Punto p1 = new(10, 20);
+p1.Display();
+ModificarPunto(ref p1);
+p1.Display();
+
+static void ModificarPunto(ref Punto punto) { punto.X = 9999; }
+struct Punto
+{
+  public int X;
+  public int Y;
+  public Punto(int pX, int pY) { X = pX; Y = pY; }
+  public readonly void Display() { Console.WriteLine($"X: {X}, Y: {Y}"); }
+}
